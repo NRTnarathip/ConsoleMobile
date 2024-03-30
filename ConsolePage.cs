ecef0b1354a4fdd205019a5328e756f3da0a2a93
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-03-30 07:05:03 +0700

    Add: LogLevel Filter & Color

--- ConsolePage.cs
+++ ConsolePage.cs
@@ -4,89 +4,132 @@ using StardewModdingAPI.AndroidExtensions;
 using StardewValley;
 using StardewValley.Menus;
 
-namespace ConsoleMobile
+namespace ConsoleMobile;
+
+public class ConsolePage : IClickableMenu
 {
-    internal class ConsolePage : IClickableMenu
+    public class LogFilter
     {
-        MobileScrollbar scrollbar;
-        public float scrollPercent;
-        public ConsolePage()
+        public LogFilter() { }
+        public bool info = true;
+        public bool trace;
+        public bool debug = true;
+        public bool error = true;
+        public bool warn = true;
+        public bool alert = true;
+        public bool IsEnable(string levelName)
         {
-            initializeUpperRightCloseButton();
-
-            var scrollbarPaddingHeight = 100;
-            scrollbar = new MobileScrollbar(viewport.Width - 100, scrollbarPaddingHeight / 2, 0, viewport.Height - scrollbarPaddingHeight);
-            scrollbar.setPercentage(0);
-        }
-        public override void leftClickHeld(int x, int y)
-        {
-            base.leftClickHeld(x, y);
-            var distFromY = y - scrollbar.Bounds.Y;
-            scrollPercent = (distFromY / (float)scrollbar.Bounds.Height) * 100;
-            scrollPercent = Math.Clamp(scrollPercent, 0, 100);
-            scrollbar.setPercentage(scrollPercent);
-        }
-        public override void draw(SpriteBatch b)
-        {
-            //draw BG
-            const int backgroundDark = 50;
-            drawTextureBox(b, 0, 0, viewport.Width, viewport.Height, new Color(backgroundDark, backgroundDark, backgroundDark));
-            base.draw(b); //draw close button
-            var st = System.Diagnostics.Stopwatch.StartNew();
-            drawLines(b);
-            st.Stop();
-            //1.2ms
-            ModEntry.Instance.Monitor.Log("use time: " + st.Elapsed.TotalMilliseconds + "ms");
-
-            scrollbar.draw(b);
-        }
-        Color InfoColor = Color.White;
-        Color DebugColor = new Color(220, 220, 220);
-        Color ErrorColor = new Color(255, 60, 60);
-        Color WarnColor = new Color(255, 120, 0);
-        Color AlertColor = new Color(200, 0, 255);
-        Vector2 ExampleLineSize = Vector2.Zero;
-        void drawLines(SpriteBatch batch)
-        {
-            var font = Game1.smallFont;
-            if (ExampleLineSize == Vector2.Zero)
-                ExampleLineSize = font.MeasureString("example line");
-
-            int paddingTextRenderY = (int)ExampleLineSize.Y * 4;
-            var lastTextPosRender = Vector2.Zero;
-            lastTextPosRender.X = 50;
-            lastTextPosRender.Y = paddingTextRenderY;
-
-            var logLines = ConsoleHook.Lines;
-            logLines.Add("[>> End Line <<]");
-
-            int totalRenderLineHeight = 0;
-            //adjust with scrollbar
-            foreach (var line in logLines)
+            return levelName.ToLower() switch
             {
-                var lineSize = font.MeasureString(line);
-                totalRenderLineHeight += (int)lineSize.Y;
+                "info" => info,
+                "trace" => trace,
+                "debug" => debug,
+                "error" => error,
+                "warn" => warn,
+                "alert" => alert,
+                _ => true
+            };
+        }
+    }
+
+    MobileScrollbar scrollbar;
+    public float scrollPercent;
+    Vector2 paddingLogRender = Vector2.Zero;
+    Vector2 ExampleLineSize = Vector2.Zero;
+    SpriteFont font;
+    const int scrollbarPaddingHeight = 150;
+    LogFilter logFilter = new();
+    public ConsolePage()
+    {
+        initializeUpperRightCloseButton();
+        upperRightCloseButton.bounds.X = 50;
+
+        font = Game1.dialogueFont;
+        ExampleLineSize = font.MeasureString("example line");
+        //var scrollbarX = viewport.Width - 100;// Right Side
+        var scrollbarX = 10; //Left Side
+        paddingLogRender.Y = ExampleLineSize.Y * 1;
+
+        scrollbar = new MobileScrollbar(scrollbarX, scrollbarPaddingHeight / 2, 40, viewport.Height - scrollbarPaddingHeight);
+        paddingLogRender.X = scrollbar.slider.bounds.X + scrollbar.slider.bounds.Width + 20;
+        scrollbar.setPercentage(0);
+    }
+    public override void leftClickHeld(int x, int y)
+    {
+        base.leftClickHeld(x, y);
+        var distFromY = y - scrollbar.Bounds.Y;
+        scrollPercent = (distFromY / (float)scrollbar.Bounds.Height) * 100;
+        scrollPercent = Math.Clamp(scrollPercent, 0, 100);
+        scrollbar.setPercentage(scrollPercent);
+    }
+    public override void draw(SpriteBatch b)
+    {
+        //draw BG
+        const int backgroundDark = 50;
+        drawTextureBox(b, 0, 0, viewport.Width, viewport.Height, new Color(backgroundDark, backgroundDark, backgroundDark));
+        drawLogs(b);
+        scrollbar.draw(b);
+        base.draw(b); //draw close button
+    }
+    Color InfoColor = Color.White;
+    Color DebugColor = new Color(220, 220, 220);
+    Color ErrorColor = new Color(255, 60, 60);
+    Color WarnColor = new Color(255, 120, 0);
+    Color AlertColor = new Color(200, 0, 255);
+    void drawLogs(SpriteBatch batch)
+    {
+
+        var currentDrawTextPos = paddingLogRender;
+
+        var LogLines = ConsoleHook.Lines;
+        LogLines.Add("[>> End Line <<]");
+
+        //adjust with scrollbar
+        //filter logs
+        int totalRenderLineHeight = 0;
+        List<Tuple<string, string>> logs = new();
+        foreach (var line in LogLines)
+        {
+            var splitLines = line.Split(" ");
+            var textLogLevel = "";
+            if (splitLines.Length >= 2)
+            {
+                //split result, 1="[04:02:08", 2="TRACT", 3=mod name
+                textLogLevel = splitLines[1];
+                bool isRender = logFilter.IsEnable(textLogLevel);
+                if (!isRender)
+                    continue;
             }
-            lastTextPosRender.Y -= (int)((scrollPercent / 100f) * (totalRenderLineHeight - Game1.viewport.Height + (paddingTextRenderY * 2)));
+            var lineSize = font.MeasureString(line);
+            totalRenderLineHeight += (int)lineSize.Y;
+            logs.Add(new(textLogLevel, line));
+        }
 
-            foreach (var line in logLines)
+        currentDrawTextPos.Y -= (int)((scrollPercent / 100f) * (totalRenderLineHeight));
+        var lastLineSize = Vector2.Zero;
+        foreach (var log in logs)
+        {
+            var line = log.Item2;
+            var textLogLevel = log.Item1;
+
+            //check text is must render in viewport
+            var lineSize = font.MeasureString(line);
+            currentDrawTextPos.Y += (int)lastLineSize.Y;
+            lastLineSize = lineSize;
+
+            if (currentDrawTextPos.Y >= paddingLogRender.Y && currentDrawTextPos.Y + ExampleLineSize.Y <= Game1.viewport.Height)
             {
-                var textColor = InfoColor;
-                var textLogLevel = line.Substring(10).Split(" ")[0];
-                textColor = textLogLevel switch
+                var textColor = textLogLevel switch
                 {
                     "DEBUG" => DebugColor,
                     "ERROR" => ErrorColor,
                     "WARN" => WarnColor,
                     "ALERT" => AlertColor,
-                    "TRACT" => DebugColor,
+                    "TRACE" => DebugColor,
                     _ => InfoColor,
                 };
 
-                var lineSize = font.MeasureString(line);
-                batch.DrawString(font, line, lastTextPosRender, textColor);
-
-                lastTextPosRender.Y += (int)lineSize.Y;
+                batch.DrawString(font, line, currentDrawTextPos, textColor);
             }
         }
     }
